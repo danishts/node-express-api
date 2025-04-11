6de022555d25823c34b69cb124cfeb05663016e4
danishts <danishsaddique519@gmail.com>
2025-04-11 15:23:50 +0500

    Initial commit

---
+++ Index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const users = require('./MOCK_DATA.json');
+
+
+const app = express();
+const PORT = 8000;
+
+
+app.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+
+
+app.listen(PORT, () => {console.log(`Server is running on port : ${PORT}`);});
